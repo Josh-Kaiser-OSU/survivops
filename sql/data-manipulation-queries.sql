ededc3052c1a5b82a7080ce2668ef0c9c50baa70
Josh-Kaiser-OSU <kaiserjo@oregonstate.edu>
2020-07-31 11:43:51 -0700

    Changed the layout of the data-manipulation queries

--- sql/data-manipulation-queries.sql
+++ sql/data-manipulation-queries.sql
@@ -10,80 +10,27 @@
 
 -- Note: This file is not yet complete.
 
--- signin.html
---    CREATE
--- Add a new customer if the user signs up
+-- CREATE
 INSERT INTO `customers` (fname, lname, email, password, phone_number)
 VALUES (:fnameInput, :lnameInput, :emailInput, :passwordInput, :phone_numberInput);
 
---    READ
--- Select an existing customer if the user signs in
+INSERT INTO carts (customer_id, cart_name) VALUES (:customer_id, :cart_name);
+
+SELECT fname, lname, email, password, phone_number FROM customers WHERE customer_id = :customer_id;
+
+-- READ
+SELECT product_name, price, image FROM products WHERE category = :category;
+
 SELECT customer_id
 FROM customers
 WHERE :emailInput IN
 
---    UPDATE
+--    READ cart (Requires a join)
 
---    DELETE
-
--- index.html
---    CREATE (Creating products happens on the admin page)
-
---    READ
-SELECT (product_name, price, image) FROM products WHERE category = :category;
---    UPDATE
-UPDATE 
---    DELETE (Deleting products happens on the admin page)
-
--- product.html
---    CREATE (Creating products happens on the admin page)
-
---    READ
-
---    UPDATE
-
---    DELETE (Deleting products happens on the admin page)
-
--- cart.html
---    CREATE
-INSERT INTO carts (customer_id, cart_name) VALUES (:customer_id, :cart_name);
---    READ (Requires a join)
-SELECT carts.cart_name, products_carts.product_quantity, products.product_name, products.category, products.vendor, products.price, products.image, products.quantity_available FROM carts
-INNER JOIN products_carts ON cart_id
-INNER JOIN products ON product_id
-WHERE carts.customer_id=:customer_id;
-
---    UPDATE
-
-
---    DELETE
-
--- order.html
---    CREATE
-
---    READ
-
---    UPDATE
-
---    DELETE
-
--- contact.html (No CRUD functionality is used in the contact -- page)
-
--- account.html
---    CREATE (Creating accounts is done on the signup page)
-
---    READ
-SELECT fname, lname, email, password, phone_number FROM customers WHERE customer_id = :customer_id;
---    UPDATE
+-- UPDATE
 UPDATE customers SET email=’new-email’, password=’new-password’ WHERE user_id = :id;
---    DELETE
+
+
+-- DELETE
 DELETE FROM customers WHERE user_id = :id;
 
--- admin.html
---    CREATE
-
---    READ
-
---    UPDATE
-
---    DELETE
