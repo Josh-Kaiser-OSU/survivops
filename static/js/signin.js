6669beae1c1b3035637ce824b329d989a43a53ab
timsyoon <timsyoon@gmail.com>
2020-08-05 19:38:54 -0700

    Update signin.js

--- static/js/signin.js
+++ static/js/signin.js
@@ -3,26 +3,26 @@ Some of the code in this file is courtesy of CS 290 course materials, especially
 Timothy Yoon's HW Assignment 6. Such code includes:
 a. Creating and sending HTTP requests
 (https://eecs.oregonstate.edu/ecampus-video/CS290/core-content/ajax-forms/js-http.html)
+b. Creating and using promises
 */
 
 const req_url = "./signin";
 
 // Add click event listeners to all buttons on the page once the HTML is
 // loaded and parsed
-document.addEventListener('DOMContentLoaded', addBtnListeners);
+document.addEventListener('DOMContentLoaded', signInListener);
+// document.addEventListener('DOMContentLoaded', signUpListener);  // todo
 
-function addBtnListeners() {
-  // Get the sign in and sign up buttons
+function signInListener() {
   let sign_in_button = document.getElementById('sign-in-button');
-  let sign_up_button = document.getElementById('sign-up-button');
+  sign_in_button.addEventListener('click', requestUserSignIn);
+}
 
-  // Add click event listener to the sign in button
-  sign_in_button.addEventListener('click', function() {
-    // Send a GET request to the server
-    let req = new XMLHttpRequest();
-    req.open('GET', req_url, false);
-    //
-    // Todo
-    //
-  });
+function requestUserSignIn {
+  // Send a GET request to the server
+  let req = new XMLHttpRequest();
+  req.open('GET', req_url, false);
+  req.setRequestHeader('Content-Type', 'application/json');
+  req.send(null);
+  // todo
 }
