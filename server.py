8cc5c655fa36129947ca755ba31e5ff09323c188
Joshua Kaiser <kaiserjo@flip1.engr.oregonstate.edu>
2020-08-05 19:22:02 -0700

    Made / and /cart populated by db data

--- server.py
+++ server.py
@@ -4,17 +4,19 @@ from db_connector.db_connector import connect_to_database, execute_query
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def home():
-    print("Fetching and rendering products web page")
-    db_connection = connect_to_database()
-    query = "SELECT product_name, category, vendor, price, image, quantity_available FROM `products`;"
-    result = execute_query(db_connection, query).fetchall()
-    categories = []
-    for r in result:
-        categories.append(r[1])
-    print(categories)
-    return render_template("index.html", rows=result, categories=categories)
+    if request.method == 'GET':
+        print("Fetching and rendering products web page")
+        db_connection = connect_to_database()
+        query = "SELECT product_name, category, vendor, price, image, quantity_available FROM `products`;"
+        result = execute_query(db_connection, query).fetchall()
+        categories = set()
+        for r in result:
+            categories.add(r[1])
+        categories = list(categories)
+        print(categories)
+        return render_template("index.html", rows=result, categories=categories)
 
 @app.route("/signin")
 def signin():
@@ -26,7 +28,19 @@ def product():
 
 @app.route("/cart")
 def cart():
-    return render_template("cart.html")
+    print("Fetching and rendering products web page")
+    db_connection = connect_to_database()
+    # Will get customer_id based on user input later
+    customer_id = 2
+    query = ''.join(["SELECT carts.cart_name AS cart_name, products.product_name AS product_name, ",
+    "products.price AS price, cart_item.product_quantity AS quantity ",
+    "FROM (SELECT cart_id, customer_id, cart_name FROM `carts` WHERE customer_id = ",
+    str(customer_id),
+    ") AS carts INNER JOIN `products_carts` AS cart_item ON carts.cart_id = cart_item.cart_id ",
+    "INNER JOIN `products` ON products.product_id = cart_item.product_id;"])
+    result = execute_query(db_connection, query).fetchall()
+    print(result)
+    return render_template("cart.html", cartitems=result)
 
 @app.route("/order")
 def order():
