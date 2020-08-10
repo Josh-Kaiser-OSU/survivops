2d636168181499c51a6b4332f063f01474807ec3
timsyoon <timsyoon@gmail.com>
2020-08-10 14:59:39 -0700

    Add functions to disable form inputs and button when product is unavailable
    Create the checkProductQty and the disableForm functions that disable the quantity to
    order field, cart selection field, and add to cart button when the product quantity
    available is less than 1.

--- static/js/product.js
+++ static/js/product.js
@@ -1,1 +1,19 @@
-console.log("product.js works");
+document.addEventListener('DOMContentLoaded', checkProductQty);
+
+function checkProductQty() {
+  let qty_available = parseInt(document.getElementById('qty_available').textContent, 10);
+  // If a product is not available, disable all inputs and the submit button
+  if (qty_available < 1) {
+    disableForm();
+  }
+}
+
+function disableForm() {
+  let qty_to_order_field = document.getElementById('quantity-to-order');
+  let cart_field = document.getElementById('cart-select');
+  let submit_btn = document.getElementById('add-to-cart-btn');
+
+  qty_to_order_field.toggleAttribute('disabled');
+  cart_field.toggleAttribute('disabled');
+  submit_btn.toggleAttribute('disabled');
+}
